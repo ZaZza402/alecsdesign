cb7d26fc310c7bb1a46b697e8da416474a124ed1
ZaZza402 <ytzazza@gmail.com>
2026-07-21 16:59:38 +0200

    dded a beforeinstallprompt listener and a simple fallback that triggers the install prompt

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,7 +3,9 @@
 declare module "virtual:pwa-register" {
   export interface RegisterSWOptions {
     immediate?: boolean;
-    onRegistered?: (swRegistration: ServiceWorkerRegistration | undefined) => void;
+    onRegistered?: (
+      swRegistration: ServiceWorkerRegistration | undefined,
+    ) => void;
     onRegisterError?: (error: any) => void;
     onOfflineReady?: () => void;
     onNeedRefresh?: () => void;
